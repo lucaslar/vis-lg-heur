4baefcdc88429d77719f000f05bc450c112101ff
Lucas Larisch <s0558070@htw-berlin.de>
2019-06-27 01:03:32 +0200

    Material: Added module for defining Material imports and exports.

---
+++ src/app/modules/material.module.ts
@@ -0,0 +1,14 @@
+import {NgModule} from '@angular/core';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+
+@NgModule({
+  imports: [
+    DragDropModule
+  ],
+  exports: [
+    DragDropModule
+  ]
+})
+export class MaterialModule {
+
+}
