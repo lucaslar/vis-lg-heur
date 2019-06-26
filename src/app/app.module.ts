39970e989e61f566a3157d1623e60b5b467dbdc2
Lucas Larisch <s0558070@htw-berlin.de>
2019-06-27 01:08:56 +0200

    Updated App Module after adding components and further modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 
-import {AppRoutingModule} from './app-routing.module';
+import {AppRoutingModule} from './modules/app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {OneArmedBanditComponent} from './components/one-armed-bandit/one-armed-bandit.component';
+import {FormsModule} from '@angular/forms';
+import {ConfigContainerComponent} from './components/configuration/config-container/config-container.component';
+import {OrderComponent} from './components/configuration/order/order.component';
+import {MaterialModule} from './modules/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneArmedBanditComponent,
+    ConfigContainerComponent,
+    OrderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
