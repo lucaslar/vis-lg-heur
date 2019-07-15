57d29f95eb128bb8019ca0310e21f83db69a5367
Lucas Larisch <s0558070@htw-berlin.de>
2019-07-15 03:19:18 +0200

    Routing and components: Deleted Params component (all routing to configuration now).

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './modules/app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {OneArmedBanditComponent} from './components/one-armed-bandit/one-armed-bandit.component';
 import {FormsModule} from '@angular/forms';
 import {ConfigContainerComponent} from './components/configuration/config-container/config-container.component';
 import {OrderComponent} from './components/configuration/order/order.component';
@@ -15,7 +14,6 @@ import { NavListContentComponent } from './components/layout/nav-list-content/na
 @NgModule({
   declarations: [
     AppComponent,
-    OneArmedBanditComponent,
     ConfigContainerComponent,
     OrderComponent,
     InfoPopUpComponent,
--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,18 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {OneArmedBanditComponent} from '../components/one-armed-bandit/one-armed-bandit.component';
 import {ConfigContainerComponent} from '../components/configuration/config-container/config-container.component';
 
 const routes: Routes = [
-  {path: 'params', component: OneArmedBanditComponent},
   {path: 'configuration', component: ConfigContainerComponent},
   {
     path: '',
-    redirectTo: '/params',
+    redirectTo: '/configuration',
     pathMatch: 'full'
   },
-  // TODO: add bypassed:
-  // {path: '**', redirectTo: '/params'}
+  {path: '**', redirectTo: '/configuration'}
 ];
 
 @NgModule({
