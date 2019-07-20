406e0237c63706528540fe30105da257ac904e6b
Lucas Larisch <s0558070@htw-berlin.de>
2019-07-20 02:42:24 +0200

    Internal: Updated Module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,33 +4,45 @@ import {NgModule} from '@angular/core';
 import {AppRoutingModule} from './modules/app-routing.module';
 import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule} from '@angular/forms';
-import {ConfigContainerComponent} from './components/configuration/config-container/config-container.component';
-import {OrderComponent} from './components/configuration/order/order.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {MaterialModule} from './modules/material.module';
-import { InfoPopUpComponent } from './components/layout/info-pop-up/info-pop-up.component';
+import { InfoPopUpComponent } from './components/dialogs/info-pop-up/info-pop-up.component';
 import { NavListContentComponent } from './components/layout/nav-list-content/nav-list-content.component';
 import { WelcomeComponent } from './components/pages/welcome/welcome.component';
+import { JobsAndMachinesComponent } from './components/pages/alpha/jobs-and-machines/jobs-and-machines.component';
+import { MachineNrPopupComponent } from './components/dialogs/machine-nr-popup/machine-nr-popup.component';
+import { AboutThisAppComponent } from './components/dialogs/about-this-app/about-this-app.component';
+import { YesNoPopUpComponent } from './components/dialogs/yes-no-pop-up/yes-no-pop-up.component';
+import { IconNumberInputComponent } from './components/shared/icon-number-input/icon-number-input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ConfigContainerComponent,
-    OrderComponent,
     InfoPopUpComponent,
     NavListContentComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    JobsAndMachinesComponent,
+    MachineNrPopupComponent,
+    AboutThisAppComponent,
+    YesNoPopUpComponent,
+    IconNumberInputComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     FormsModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
-  entryComponents: [InfoPopUpComponent]
+  entryComponents: [
+    AboutThisAppComponent,
+    InfoPopUpComponent,
+    MachineNrPopupComponent,
+    YesNoPopUpComponent
+  ]
 })
 export class AppModule {
 }
