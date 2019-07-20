77039c7e6e9f28e62e4684f707454d95aba554cb
Lucas Larisch <s0558070@htw-berlin.de>
2019-07-20 12:30:46 +0200

    Updated app routing module.

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -1,17 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
-import {ConfigContainerComponent} from '../components/configuration/config-container/config-container.component';
 import {WelcomeComponent} from '../components/pages/welcome/welcome.component';
+import {JobsAndMachinesComponent} from '../components/pages/alpha/jobs-and-machines/jobs-and-machines.component';
+import {JobsTerminationComponent} from '../components/pages/beta/jobs-termination/jobs-termination.component';
+import {JobsExistGuard} from '../guards/jobs-exist.guard';
 
 const routes: Routes = [
-  {path: 'welcome', component: WelcomeComponent},
-  {path: 'configuration', component: ConfigContainerComponent},
-  {
-    path: '',
-    redirectTo: '/welcome',
-    pathMatch: 'full'
-  },
-  {path: '**', redirectTo: '/welcome'}
+  {path: 'alpha', component: JobsAndMachinesComponent},
+  {path: 'beta/jobs-termination', canLoad: [JobsExistGuard], component: JobsTerminationComponent},
+  {path: '', component: WelcomeComponent},
+  {path: '**', redirectTo: ''}
 ];
 
 @NgModule({
