05dfbda77d3247f7427251ddd866b3622536004c
Lucas Larisch <s0558070@htw-berlin.de>
2019-07-20 21:31:31 +0200

    UX: Updated number input component including dynamic feedback.

--- src/app/components/shared/icon-number-input/icon-number-input.component.ts
+++ src/app/components/shared/icon-number-input/icon-number-input.component.ts
@@ -17,21 +17,30 @@ export class IconNumberInputComponent implements OnInit {
 
   @Output() newValue: EventEmitter<number> = new EventEmitter();
 
+  isLegalValue = true;
+
   ngOnInit(): void {
-    this.iconClasses.forEach(
-      className => (<HTMLElement>this.icon.nativeElement).classList.add(className)
-    );
+    if (this.iconClasses) {
+      this.iconClasses.forEach(
+        className => (<HTMLElement>this.icon.nativeElement).classList.add(className)
+      );
+    }
+  }
+
+  checkValidityOfNumber(): void {
+    const value: number = +this.inputFieldRef.nativeElement.value;
+    this.isLegalValue = !value || !(this.min && value < this.min) && !(this.max && value > this.max);
   }
 
   onNumberChange(): void {
     const inputField: HTMLInputElement = this.inputFieldRef.nativeElement;
     const value = +inputField.value;
-    if (value) {
-      inputField.value = (value).toString();
-      this.newValue.emit(value);
-    } else {
+    if (!value) {
       inputField.value = '';
       this.newValue.emit(undefined);
+    } else if (this.isLegalValue) {
+      inputField.value = (value).toString();
+      this.newValue.emit(value);
     }
   }
 
