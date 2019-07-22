da76f13acfbbaa43d8b8d3b188976b9b370a52df
Lucas Larisch <s0558070@htw-berlin.de>
2019-07-22 16:45:02 +0200

    Internal/Routing: Beta defined as parent route for children.

--- src/app/modules/app-routing.module.ts
+++ src/app/modules/app-routing.module.ts
@@ -3,13 +3,16 @@ import {Routes, RouterModule} from '@angular/router';
 import {WelcomeComponent} from '../components/pages/welcome/welcome.component';
 import {JobsAndMachinesComponent} from '../components/pages/alpha/jobs-and-machines/jobs-and-machines.component';
 import {JobsTerminationComponent} from '../components/pages/beta/jobs-termination/jobs-termination.component';
-import {JobsExistGuard} from '../guards/jobs-exist.guard';
 import {PriorityRulesDefinitionComponent} from '../components/pages/priority-rules-definition/priority-rules-definition.component';
 
 const routes: Routes = [
   {path: 'alpha', component: JobsAndMachinesComponent},
   {path: 'priority-rules', component: PriorityRulesDefinitionComponent},
-  {path: 'beta/jobs-termination', canActivate: [JobsExistGuard], component: JobsTerminationComponent},
+  {
+    path: 'beta', children: [
+      {path: 'jobs-termination', component: JobsTerminationComponent}
+    ]
+  },
   {path: '', component: WelcomeComponent},
   {path: '**', redirectTo: ''}
 ];
