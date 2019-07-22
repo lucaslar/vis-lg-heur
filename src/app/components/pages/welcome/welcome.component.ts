c834726aa24085f637e4759ed341dadab0e5101b
Lucas Larisch <s0558070@htw-berlin.de>
2019-07-22 18:41:41 +0200

    (UI): Added layout for welcome page.

--- src/app/components/pages/welcome/welcome.component.ts
+++ src/app/components/pages/welcome/welcome.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
-  styleUrls: ['./welcome.component.css']
+  styleUrls: ['./welcome.component.css', '../pages-styles.css']
 })
-export class WelcomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class WelcomeComponent {
 
 }
