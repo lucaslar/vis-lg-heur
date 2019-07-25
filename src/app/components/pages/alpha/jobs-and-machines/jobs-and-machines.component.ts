e2e6590fe95f65766115d09118107202d09ff721
Lucas Larisch <s0558070@htw-berlin.de>
2019-07-25 10:30:36 +0200

    Internal/TODO: Added possible TODO for jobs/machine creation.

--- src/app/components/pages/alpha/jobs-and-machines/jobs-and-machines.component.ts
+++ src/app/components/pages/alpha/jobs-and-machines/jobs-and-machines.component.ts
@@ -18,6 +18,8 @@ import {PopUpComponent} from '../../../dialogs/pop-up/pop-up.component';
 })
 export class JobsAndMachinesComponent implements OnInit {
 
+  // TODO: 'Undo' in snackbar for sorting and JS->FS?
+
   private _definableValue = DefinableValue;
   private _configurationStatus = DefinitionStatus;
 
