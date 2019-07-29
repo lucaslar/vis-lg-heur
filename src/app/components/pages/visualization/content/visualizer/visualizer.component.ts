cd25d139bda7dba3817bfeb51148bf5e00400e36
Lucas Larisch <s0558070@htw-berlin.de>
2019-07-29 15:33:17 +0200

    Content/visualization: Basic structure of scheduling result page.

--- src/app/components/pages/visualization/content/visualizer/visualizer.component.ts
+++ src/app/components/pages/visualization/content/visualizer/visualizer.component.ts
@@ -11,18 +11,57 @@ import {SchedulingResult} from '../../../../../model/internal/visualization/Sche
 })
 export class VisualizerComponent implements OnInit {
 
+  // TODO: (Generally) Readonly -> Upper case
+
   private _result: SchedulingResult;
+
+  private _isGeneralDataVisible = true;
+  private _isSolutionQualityDataVisible = true;
+  private _isVisualizableGeneralDataVisible = true;
+  private _isVisualizableSolutionQualityDataVisible = true;
 
   constructor(public scheduling: SchedulingService,
               private route: ActivatedRoute) {
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
     const heuristicDefiner = <HeuristicDefiner>this.route.snapshot.paramMap.get('heuristic');
     this._result = this.scheduling.scheduleUsingHeuristic(heuristicDefiner);
   }
 
   get result(): SchedulingResult {
     return this._result;
+  }
+
+  get isGeneralDataVisible(): boolean {
+    return this._isGeneralDataVisible;
+  }
+
+  set isGeneralDataVisible(value: boolean) {
+    this._isGeneralDataVisible = value;
+  }
+
+  get isSolutionQualityDataVisible(): boolean {
+    return this._isSolutionQualityDataVisible;
+  }
+
+  set isSolutionQualityDataVisible(value: boolean) {
+    this._isSolutionQualityDataVisible = value;
+  }
+
+  get isVisualizableGeneralDataVisible(): boolean {
+    return this._isVisualizableGeneralDataVisible;
+  }
+
+  set isVisualizableGeneralDataVisible(value: boolean) {
+    this._isVisualizableGeneralDataVisible = value;
+  }
+
+  get isVisualizableSolutionQualityDataVisible(): boolean {
+    return this._isVisualizableSolutionQualityDataVisible;
+  }
+
+  set isVisualizableSolutionQualityDataVisible(value: boolean) {
+    this._isVisualizableSolutionQualityDataVisible = value;
   }
 }
