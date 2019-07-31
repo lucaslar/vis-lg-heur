d0525d330cc4c795b80cf854ab16ef175fec75b2
Lucas Larisch <s0558070@htw-berlin.de>
2019-07-31 15:32:39 +0200

    Content: Fixed typo in string value BETA_DUE_DATES.

--- src/app/model/internal/value-definition/DefinableValue.ts
+++ src/app/model/internal/value-definition/DefinableValue.ts
@@ -1,6 +1,6 @@
 export enum DefinableValue {
   ALPHA_JOB_TIMES = 'Zeiten für Arbeitsgänge',
-  BETA_DUE_DATES = 'FertigstellungsZeitpunkte',
+  BETA_DUE_DATES = 'Fertigstellungszeitpunkte',
   BETA_SETUP_TIMES = 'Rüstzeiten',
   PRIORITY_RULES = 'Prioritätsregeln'
 }
