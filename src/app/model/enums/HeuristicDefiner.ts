c62c203ca63ab0635e4bf9c4392dd9f4e27be286
Lucas Larisch <s0558070@htw-berlin.de>
2019-08-01 13:49:57 +0200

    Internal: Changed Typo for heuristic definer.

--- src/app/model/enums/HeuristicDefiner.ts
+++ src/app/model/enums/HeuristicDefiner.ts
@@ -1,5 +1,5 @@
 export enum HeuristicDefiner {
   PRIORITY_RULES = 'priority-rules',
-  NEAREST_NEIGHBOUR = 'nearest-neighbor',
+  NEAREST_NEIGHBOUR = 'nearest-neighbour',
   NEH_HEURISTIC = 'neh-heuristic'
 }
