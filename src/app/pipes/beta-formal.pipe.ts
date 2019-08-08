9d97c00dad34bc9b94bddfc5653823883cf89fc6
Lucas Larisch <s0558070@htw-berlin.de>
2019-08-08 02:06:27 +0200

    Content: Simplified beta pipe (both the algorithm and the returned value).

--- src/app/pipes/beta-formal.pipe.ts
+++ src/app/pipes/beta-formal.pipe.ts
@@ -7,29 +7,9 @@ import {Job} from '../model/Job';
 })
 export class BetaFormalPipe implements PipeTransform {
 
-  // TODO content: List all or general description only xy <sub>j/i</sub> etc.?
-
-  private static formalDueDates(jobs: Job[]): string {
-    return jobs.map(job => 'd<sub>' + job.id + '</sub>=' + job.dueDate).join(', ');
-  }
-
-  private static formalWeightings(jobs: Job[]): string {
-    return jobs.map(job => 'w<sub>' + job.id + '</sub>' + job.weight).join(', ');
-  }
-
-  private static formalSetupTimes(jobs: Job[]): string {
-    return jobs.map(job => {
-      const base = 's<sub>' + job.id + ',';
-      return job.setupTimesToOtherJobs
-        .map(_job => base + _job.idTo + '</sub>=' + _job.duration)
-        .join(', ');
-    }).join(', ');
-  }
-
   transform(value: Job[], objectiveFunction: ObjectiveFunction): string {
 
-    let betaFormal = '';
-    value = value.sort((j1, j2) => j1.id - j2.id);
+    const betaFormal: string[] = [];
 
     // Add due dates:
     if (objectiveFunction === ObjectiveFunction.SUM_DEADLINE_EXCEEDANCES
@@ -38,7 +18,7 @@ export class BetaFormalPipe implements PipeTransform {
       || objectiveFunction === ObjectiveFunction.WEIGHTED_NUMBER_DEADLINE_EXCEEDANCES
       || objectiveFunction === ObjectiveFunction.SUM_DELAYED_WORK
       || objectiveFunction === ObjectiveFunction.SUM_WEIGHTED_DELAYED_WORK) {
-      betaFormal += BetaFormalPipe.formalDueDates(value);
+      betaFormal.push('d<sub>j</sub>');
     }
 
     // Add weights:
@@ -46,20 +26,14 @@ export class BetaFormalPipe implements PipeTransform {
       || objectiveFunction === ObjectiveFunction.SUM_WEIGHTED_FINISHING_TIMESTAMPS
       || objectiveFunction === ObjectiveFunction.SUM_WEIGHTED_DELAYED_WORK
       || objectiveFunction === ObjectiveFunction.SUM_WEIGHTED_DEADLINE_EXCEEDANCES) {
-      if (betaFormal) {
-        betaFormal += ', ';
-      }
-      betaFormal += BetaFormalPipe.formalWeightings(value);
+      betaFormal.push('w<sub>j</sub>');
     }
 
     // Add setup times:
     if (objectiveFunction === ObjectiveFunction.SUM_SETUP_TIME) {
-      if (betaFormal) {
-        betaFormal += ', ';
-      }
-      betaFormal += BetaFormalPipe.formalSetupTimes(value);
+      betaFormal.push('s<sub>jk</sub>');
     }
 
-    return betaFormal;
+    return betaFormal.join(', ');
   }
 }
