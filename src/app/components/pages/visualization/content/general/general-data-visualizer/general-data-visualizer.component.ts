d29946bc43a656db8de28e6e03c3b2f2d4d01528
Lucas Larisch <s0558070@htw-berlin.de>
2019-08-09 05:54:56 +0200

    Content: Added visualized general data info text.

--- src/app/components/pages/visualization/content/general/general-data-visualizer/general-data-visualizer.component.ts
+++ src/app/components/pages/visualization/content/general/general-data-visualizer/general-data-visualizer.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {VisualizableGeneralData} from '../../../../../../model/internal/visualization/SchedulingResult';
+import {ObjectiveFunction} from '../../../../../../model/enums/ObjectiveFunction';
 
 @Component({
   selector: 'app-general-data-visualizer',
@@ -9,5 +10,16 @@ import {VisualizableGeneralData} from '../../../../../../model/internal/visualiz
 export class GeneralDataVisualizerComponent {
 
   @Input() data: VisualizableGeneralData;
+  @Input() objectiveFunction: ObjectiveFunction;
+
+  isDueDatesConsidered(): boolean {
+    return (this.objectiveFunction === ObjectiveFunction.SUM_DEADLINE_EXCEEDANCES
+      || this.objectiveFunction === ObjectiveFunction.SUM_WEIGHTED_DEADLINE_EXCEEDANCES
+      || this.objectiveFunction === ObjectiveFunction.NUMBER_DEADLINE_EXCEEDANCES
+      || this.objectiveFunction === ObjectiveFunction.WEIGHTED_NUMBER_DEADLINE_EXCEEDANCES
+      || this.objectiveFunction === ObjectiveFunction.SUM_DELAYED_WORK
+      || this.objectiveFunction === ObjectiveFunction.SUM_WEIGHTED_DELAYED_WORK
+      || this.objectiveFunction === ObjectiveFunction.MAX_DELAY);
+  }
 
 }
