f70a7a19a669972ccca6e5c2de5e84ebcf5dd6a4
Lucas Larisch <s0558070@htw-berlin.de>
2019-08-09 05:17:51 +0200

    Content: Added general data content.

--- src/app/components/pages/visualization/content/general/general-data/general-data.component.ts
+++ src/app/components/pages/visualization/content/general/general-data/general-data.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input} from '@angular/core';
 import {GeneralSchedulingData} from '../../../../../../model/internal/visualization/SchedulingResult';
+import {Heuristic} from '../../../../../../model/Heuristic';
+import {MachineConfig} from '../../../../../../model/enums/MachineConfig';
 
 @Component({
   selector: 'app-general-data',
@@ -9,5 +11,12 @@ import {GeneralSchedulingData} from '../../../../../../model/internal/visualizat
 export class GeneralDataComponent {
 
   @Input() data: GeneralSchedulingData;
+  @Input() heuristic: Heuristic;
 
+  private readonly _machineCongig = MachineConfig;
+
+
+  get machineCongig(): any {
+    return this._machineCongig;
+  }
 }
